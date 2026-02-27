6e9943e28bbdb405547c9aee61db0551d03721d9
unlimitedCodeG <1659159807@qq.com>
2026-02-27 14:33:16 +0800

    feat:add main.rs by salvo

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,19 @@
-fn main() {
-    let raw_ptr: *const String;
+use salvo::prelude::*;
 
-    // 1. 在一个小作用域内分配内存
-    {
-        let s = Box::new(String::from("这是原始的机密字符串"));
-        raw_ptr = &*s as *const String; // 取得指向堆内存的裸指针
-        println!("【1】数据已分配，地址: {:?}", raw_ptr);
-        // s 在这里超出作用域，内存被 free
-    } 
+mod routes;
 
-    println!("【2】s 已经被释放，但 raw_ptr 仍然指向那个地址（悬空指针）");
 
-    // 2. 模拟内存重用：立即分配一个完全不同的对象
-    // 操作系统可能会把刚刚释放的那块内存分配给这个新变量
-    let _tamper = Box::new(vec![123456789, 987654321]); 
-    println!("【3】申请了新内存（一个数组），可能重用了之前的空间");
+#[tokio::main]
+async fn main() {
+    // 初始化日志
+    tracing_subscriber::fmt().init();
 
-    // 3. 触发 Use-After-Free
-    println!("--- 准备触发非法访问 ---");
-    unsafe {
-        // 此时 raw_ptr 试图把那一串数字当成 String 来解析
-        // 这会导致乱码、内存访问违规或程序崩溃
-        println!("【4】读取 UAF 指针内容: {}", *raw_ptr); 
-    }
-    unsafe {
-        // 尝试往一个已经“死亡”的地址写数据
-        // 此时 raw_ptr 指向的可能已经是系统的关键数据结构或只读区域
-        let mut_ptr = raw_ptr as *mut String;
-        *mut_ptr = String::from("丢你楼母"); 
-        println!("成功写入？ {}", *mut_ptr);
-    }
+    let router = routes::init_router();
+
+
+    print!("🚀 服务启动：http://127.0.0.1:5800");
+
+
+    let acceptor  =TcpListener::new("0.0.0.0:5800").bind().await;
+    Server::new(acceptor).serve(router).await;
 }
